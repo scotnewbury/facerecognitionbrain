4377a15c5745f1f538f9a0276f31ad549ad19092
scotnewbury <scot@scotnewbury.com>
2019-06-30 23:08:25 -0400

    Added initial state to clear user info

--- src/App.js
+++ src/App.js
@@ -10,24 +10,25 @@ import SignIn from './components/SignIn/SignIn'
 import Register from './components/Register/Register'
 import './App.css'
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+  }
+}
+
 export default class App extends Component {
-  
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState
   }
 
   loadUser = (data) => {
@@ -78,7 +79,9 @@ export default class App extends Component {
           .then(response => response.json())
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
-        })}
+        })
+        .catch(console.log)
+      }
         this.displayFaceBox(this.calculateFaceLocation (response))
       })
       .catch(err => console.log(err))
@@ -86,7 +89,7 @@ export default class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
